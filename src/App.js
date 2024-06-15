44ec698888820ec209df0bf0d0e623d2e4f05894
sasi1206 <sasidharankm4@gmail.com>
2024-06-15 22:38:59 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -4,57 +4,34 @@ import { useState , useEffect} from "react";
 function App() {
   const[rangeValue,setRangeValue]=useState(1);
   const[pass,setPass] = useState('')
-  const CharaterArray = [
+  const[Options,setOptions]=useState([
     {
       Id:1,
-      OptionName:"Upper Case",
-      CharCode: [65,90]
+      OptionName:"Upper Case"
     },
     {
       Id:2,
-      OptionName:"Lower Case",
-      CharCode: [97,122]
+      OptionName:"Lower Case"
     },
     {
       Id:3,
-      OptionName: "Special Characters",
-      CharCode: [33,47]
+      OptionName:"Special Characters"
     },
     {
       Id:4,
-      OptionName: "Numbers",
-      CharCode: [48,57]
+      OptionName:"Numbers"
     }
-  ];
-  const Options = [
-    {
-      Id:1,
-      OptionName:"Upper Case",
-      checked: false
-    },
-    {
-      Id:2,
-      OptionName:"Lower Case",
-      checked: false
-    },
-    {
-      Id:3,
-      OptionName:"Special Charaters",
-      checked: false
-    },
-    {
-      Id:4,
-      OptionName:"Numbers",
-      checked: false
-    }
-  ];
+  ]);
+  const[UpperCase,setUpperCase] = useState(false);
+  const[LowerCase,setLowerCase] = useState(false);
+  const[Numbers,setNumbers] = useState(false);
+  const[SpecialCharacters,setSpecialCharacters] = useState(false);
   let string ='';
   const border = document.querySelector(".input input");
 
   useEffect(()=>{
     if(border){
-      if(rangeValue == 1 )border.style.border = "1px solid black";
-      else if(rangeValue <= 5 && rangeValue > 1) border.style.border = "2px solid red";
+      if(rangeValue <= 5) border.style.border = "2px solid red";
       else if(rangeValue <=10 && rangeValue > 5) border.style.border = "2px solid #fffe7a";
       else if(rangeValue <=15 && rangeValue > 10) border.style.border = "2px solid lime";
       else border.style.border = "2px solid #B8252A";
@@ -62,16 +39,25 @@ function App() {
   },[rangeValue])
 
   const handleCheck = (id)=>{
-    console.log(id);
+    const filterArray = Options.find((option)=> option.Id === id);
+   return filterArray.OptionName == 'Upper Case' ? setUpperCase(!UpperCase) : filterArray.OptionName == 'Lower Case' ? setLowerCase(!LowerCase) : filterArray.OptionName == 'Special Characters' ? setSpecialCharacters(!SpecialCharacters) : filterArray.OptionName == 'Numbers' ? setNumbers(!Numbers) : "nope";
   }
 
-  const GeneratePassword=(min,max)=>{
+  const GeneratePassword=(min,max,UnwantedValueStart,UnwantedValueStop)=>{
+
     for(let i=1;i<=rangeValue;i++){
       const rand = Math.floor(Math.random() * (max - min + 1) + min);
+    if(rand >= UnwantedValueStart  && rand <= UnwantedValueStop){ console.log(UnwantedValueStart,UnwantedValueStop)}
       const char = String.fromCharCode(rand);
       string=string + char;
     }
     setPass(string)
+  }
+
+  const MainFunction = ()=>{
+      if(UpperCase && LowerCase && SpecialCharacters && Numbers) GeneratePassword(21,122);
+      else if(Numbers && LowerCase ) GeneratePassword(48,122,58,96);
+      else setPass('')
   }
 
   return (
@@ -88,28 +74,34 @@ function App() {
             <button><FaCopy/></button>
           </section>
           <section className="slider">
-            <label>Password Length</label>
-            <input 
+            <label >Password Length</label>
+            <input
+              key={1}
               type="range"
               onChange={(e)=>setRangeValue(e.target.value)}
               value={rangeValue}
               min={1}
               max={20}
-            /><p>{rangeValue}</p>
+            /><p key={2}>{rangeValue}</p>
           </section>
           <section className="options">
             <label className="tit">Password Options</label>
-            <table>
-              <tbody>
-                  <tr>
-                      
-                  </tr>
-              </tbody>
-            </table>
+            {
+              Options.map((option)=>
+                <>
+                  <input 
+                    key={option.Id}
+                    type="checkbox"
+                    onChange={()=> handleCheck(option.Id)}
+                  />
+                  <label>{option.OptionName}</label>
+                </>
+              )
+            }
           </section>
           <button 
             className="gen"
-            onClick={()=>GeneratePassword(65,90)}  
+            onClick={MainFunction}  
           >Generate Password</button>
         </form>
       </main>
